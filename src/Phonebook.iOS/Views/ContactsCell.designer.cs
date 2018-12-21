3bb5f9e432b34bdf6427fd3f6443cbc2c668aa8c
v.osenchuk <v.osenchuk@DEV-DC-1D>
2018-12-21 15:58:12 +0700

    hotfix

---
+++ src/Phonebook.iOS/Views/ContactsCell.designer.cs
@@ -0,0 +1,33 @@
+﻿using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace Phonebook.iOS.Views
+{
+    [Register("ContactsCell")]
+    partial class ContactsCell
+    {
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        FFImageLoading.Cross.MvxCachedImageView Image { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
+        UIKit.UILabel Title { get; set; }
+
+        void ReleaseDesignerOutlets()
+        {
+            if (Image != null)
+            {
+                Image.Dispose();
+                Image = null;
+            }
+
+            if (Title != null)
+            {
+                Title.Dispose();
+                Title = null;
+            }
+        }
+    }
+}
